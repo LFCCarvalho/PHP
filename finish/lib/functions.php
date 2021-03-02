8fc2018a391900e2db0fc11f7f3eadc09fbab070
Luis Carvalho <luisfernando@ciandt.com>
2021-03-02 13:14:08 -0300

    fix bug on 1 digit

--- finish/lib/functions.php
+++ finish/lib/functions.php
@@ -34,11 +34,14 @@ function is_valid_cpf($cpf)
     $currentCPF = substr($cpf, 0, 9);
     echo $currentCPF . PHP_EOL;
     $digit1 = 11 - (cpf_helper($currentCPF, 10) % 11);
-    
+    if($digit1 > 9) {
+        $digit1 = 0;
+    }
+
     $currentCPF = $currentCPF . $digit1;
 
     $digit2 = 11 - (cpf_helper($currentCPF, 11) % 11);
-    if($digit2 > 9){ // resto da divisão é igual a 0 ou 1
+    if($digit2 > 9) { // resto da divisão é igual a 0 ou 1
         $digit2 = 0;
     }
 
