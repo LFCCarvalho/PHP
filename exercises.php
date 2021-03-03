aef9511ae5fe1cb08c0469de214179e406e341b1
Luis Carvalho <luisfernando@ciandt.com>
2021-03-03 10:59:57 -0300

    fix some code sniffer violations

--- exercises.php
+++ exercises.php
@@ -1,31 +1,37 @@
 <?php
-function media(...$number) {
+function media(...$number)
+{
     $sum = 0;
-    foreach($number as $num) {
+    foreach ($number as $num) {
         $sum += $num;
     }
     return $sum / count($number);
 }
 echo media(5, 3, 2, 1, 16, 92) . PHP_EOL;
 
-function celsius_to_farenheit($celsius) {
+function celsiusToFarenheit($celsius)
+{
     return ($celsius * 1.8 + 32);
 }
-echo celsius_to_farenheit(100) . PHP_EOL;
+echo celsiusToFarenheit(100) . PHP_EOL;
 
-function meter_to_centimeter($meters) {
+function metersToCentimeters($meters)
+{
     return $meters * 100;
 }
-echo meter_to_centimeter(0.53) . PHP_EOL;
+echo metersToCentimeters(0.53) . PHP_EOL;
 
-function print_bigger_smaller(...$numbers) {
+function printBiggerSmaller(...$numbers) 
+{
     echo max($numbers) . " / " . min($numbers);
 }
-echo print_bigger_smaller(5, 3, 1, 0, -62, 165, 164, 6, -1, -99) . PHP_EOL;
+echo printBiggerSmaller(5, 3, 1, 0, -62, 165, 164, 6, -1, -99) . PHP_EOL;
 
-function week_day($number) {
-    $week = array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado");
-    if($number > 0 && $number < 8) {
+function week_day($number) 
+{
+    $week = array("Domingo", "Segunda", "Terça", "Quarta",
+     "Quinta", "Sexta", "Sabado");
+    if ($number > 0 && $number < 8) {
         echo $week[($number-1)];
     } else {
         echo "Valor inválido";
@@ -33,16 +39,17 @@ function week_day($number) {
 }
 echo week_day(7) . PHP_EOL;
 
-function is_triangle($l1, $l2, $l3) {
-    if($l1 == 0 || $l2 == 0 || $l3 == 0){
+function isTriangle($l1, $l2, $l3)
+{
+    if ($l1 == 0 || $l2 == 0 || $l3 == 0) {
         echo "Não pode ser um triangulo";
         return;    
     }
 
-    if(($l1 + $l2 > $l3) || ($l2 + $l3 > $l1) || ($l1 + $l3 > $l2)) {
-        if($l1 == $l2 && $l2 == $l3) {
+    if (($l1 + $l2 > $l3) || ($l2 + $l3 > $l1) || ($l1 + $l3 > $l2)) {
+        if ($l1 == $l2 && $l2 == $l3) {
             echo "Os lados formam um triângulo Equilátero";
-        } else if($l1 == $l2 || $l1 == $l3 || $l2 == $l3) {
+        } else if ($l1 == $l2 || $l1 == $l3 || $l2 == $l3) {
             echo "Os lados formam um triângulo Isósceles";
         } else {
             echo "Os lados formam um triângulo Escaleno";
@@ -51,6 +58,5 @@ function is_triangle($l1, $l2, $l3) {
         echo "Não pode ser um triangulo";
     }
 }
-
-is_triangle(0, 0, 0) . PHP_EOL;
+isTriangle(0, 0, 0) . PHP_EOL;
 echo "Teste";
